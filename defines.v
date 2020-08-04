ee58ba6ef5c8a5054834fc53da3d951cc914831d
yrralh <295979082@qq.com>
2020-08-04 16:37:34 +0800

    clear the file define.v

--- defines.v
+++ defines.v
@@ -1,36 +1,35 @@
 `timescale 1ns / 1ps
 
-/*------------------- ȫ�ֲ��� -------------------*/
-`define RST_ENABLE      1'b0                // ��λ�ź���Ч  RST_ENABLE
-`define RST_DISABLE     1'b1                // ��λ�ź���Ч
-`define ZERO_WORD       32'h00000000        // 32λ����ֵ0
-`define ZERO_DWORD      64'b0               // 64λ����ֵ0
-`define WRITE_ENABLE    1'b1                // ʹ��д
-`define WRITE_DISABLE   1'b0                // ��ֹд
-`define READ_ENABLE     1'b1                // ʹ�ܶ�
-`define READ_DISABLE    1'b0                // ��ֹ��
-`define ALUOP_BUS       7 : 0               // ����׶ε����aluop_o�Ŀ���
-`define SHIFT_ENABLE    1'b1                // ��λָ��ʹ�� 
-`define ALUTYPE_BUS     2 : 0               // ����׶ε����alutype_o�Ŀ���  
-`define TRUE_V          1'b1                // �߼�"��"  
-`define FALSE_V         1'b0                // �߼�"��"  
-`define CHIP_ENABLE     1'b1                // оƬʹ��  
-`define CHIP_DISABLE    1'b0                // оƬ��ֹ  
-`define WORD_BUS        31: 0               // 32λ��
-`define DOUBLE_REG_BUS  63: 0               // ������ͨ�üĴ����������߿���
-`define RT_ENABLE       1'b1                // rtѡ��ʹ��
-`define SIGNED_EXT      1'b1                // ������չʹ��
-`define IMM_ENABLE      1'b1                // ������ѡ��ʹ��
-`define UPPER_ENABLE    1'b1                // ��������λʹ��
-`define MREG_ENABLE     1'b1                // д�ؽ׶δ洢�����ѡ���ź�
-`define BSEL_BUS        3 : 0               // ���ݴ洢���ֽ�ѡ���źſ���
-`define PC_INIT         32'hbfc00000        // PC��ʼֵ
 
-/*------------------- ָ���ֲ��� -------------------*/
-`define INST_ADDR_BUS   31: 0               // ָ��ĵ�ַ����
-`define INST_BUS        31: 0               // ָ������ݿ���
+`define RST_ENABLE      1'b0                
+`define RST_DISABLE     1'b1                
+`define ZERO_WORD       32'h00000000        
+`define ZERO_DWORD      64'b0               
+`define WRITE_ENABLE    1'b1                
+`define WRITE_DISABLE   1'b0                
+`define READ_ENABLE     1'b1                
+`define READ_DISABLE    1'b0                
+`define ALUOP_BUS       7 : 0               
+`define SHIFT_ENABLE    1'b1                
+`define ALUTYPE_BUS     2 : 0               
+`define TRUE_V          1'b1                
+`define FALSE_V         1'b0                
+`define CHIP_ENABLE     1'b1                
+`define CHIP_DISABLE    1'b0                
+`define WORD_BUS        31: 0               
+`define DOUBLE_REG_BUS  63: 0               
+`define RT_ENABLE       1'b1                
+`define SIGNED_EXT      1'b1                
+`define IMM_ENABLE      1'b1                
+`define UPPER_ENABLE    1'b1                
+`define MREG_ENABLE     1'b1                
+`define BSEL_BUS        3 : 0               
+`define PC_INIT         32'hbfc00000        
 
-// ��������alutype
+`define INST_ADDR_BUS   31: 0               
+`define INST_BUS        31: 0               
+
+
 `define NOP             3'b000
 `define ARITH           3'b001
 `define LOGIC           3'b010
@@ -39,7 +38,7 @@
 `define JUMP            3'b101
 `define PRIVILEGE       3'B110
 
-// �ڲ�������aluop
+
 `define MINIMIPS32_ADD             8'h01
 `define MINIMIPS32_ADDI            8'h02
 `define MINIMIPS32_ADDU            8'h03
@@ -98,11 +97,10 @@
 `define MINIMIPS32_MFC0            8'h38
 `define MINIMIPS32_MTC0            8'h39
 
-/*------------------- ͨ�üĴ����Ѳ��� -------------------*/
-`define REG_BUS         31: 0               // �Ĵ������ݿ���
-`define REG_ADDR_BUS    4 : 0               // �Ĵ����ĵ�ַ����
-`define REG_NUM         32                  // �Ĵ�������32��
-`define REG_NOP         5'b00000            // ��żĴ���
+`define REG_BUS         31: 0               
+`define REG_ADDR_BUS    4 : 0               
+`define REG_NUM         32                  
+`define REG_NOP         5'b00000            
 
 
 
@@ -113,49 +111,41 @@
 
 
 
+`define STALL_BUS       4 : 0               
+`define STOP            1'b1                
+`define NOSTOP          1'b0                
 
-/************************��������ˮ����ͣ begin******************************/
-/*------------------- ��ˮ����ͣ -------------------*/
-`define STALL_BUS       4 : 0               // ��ͣ�źſ���
-`define STOP            1'b1                // ��ˮ����ͣ
-`define NOSTOP          1'b0                // ��ˮ�߲���ͣ
+`define DIV_FREE            2'b00           
+`define DIV_BY_ZERO         2'b01           
+`define DIV_ON              2'b10           
+`define DIV_END             2'b11           
+`define DIV_READY           1'b1            
+`define DIV_NOT_READY       1'b0            
+`define DIV_START           1'b1            
+`define DIV_STOP            1'b0            
 
-/*------------------- ����ָ����� -------------------*/
-`define DIV_FREE            2'b00           // ����׼��״̬
-`define DIV_BY_ZERO         2'b01           // �ж��Ƿ����״̬
-`define DIV_ON              2'b10           // ������ʼ״̬
-`define DIV_END             2'b11           // ��������״̬
-`define DIV_READY           1'b1            // ������������ź�
-`define DIV_NOT_READY       1'b0            // ��������δ�����ź�
-`define DIV_START           1'b1            // ������ʼ�ź�
-`define DIV_STOP            1'b0            // ����δ��ʼ�ź�
-/************************��������ˮ����ͣ end*********************************/
+`define CP0_INT_BUS         5 : 0           
+`define CP0_BADVADDR        8               
+`define CP0_STATUS          12              
+`define CP0_CAUSE           13              
+`define CP0_EPC             14              
 
-/************************�쳣���� begin*******************************/
-/*------------------- CP0���� -------------------*/
-`define CP0_INT_BUS         5 : 0           // �ж��źŵĿ���
-`define CP0_BADVADDR        8               // BadVAddr�Ĵ�����ַ����ţ�
-`define CP0_STATUS          12              // Status�Ĵ�����ַ����ţ�
-`define CP0_CAUSE           13              // Cause�Ĵ�����ַ����ţ�
-`define CP0_EPC             14              // EPC�Ĵ�����ַ����ţ�
+`define EXC_CODE_BUS        4 : 0           
+`define EXC_INT             5'b00           
+`define EXC_ADEL            5'h04           
+`define EXC_ADES            5'h05           
+`define EXC_SYS             5'h08           
+`define EXC_BP              5'h09           
+`define EXC_RI              5'h0a           
+`define EXC_OV              5'h0c           
+`define EXC_NONE            5'h10           
+`define EXC_ERET            5'h11           
+`define EXC_ADDR            32'hbfc00380    
+`define EXC_INT_ADDR        32'hbfc00380    
 
-/*------------------- �쳣�������� -------------------*/
-`define EXC_CODE_BUS        4 : 0           // �쳣�����������ݿ���
-`define EXC_INT             5'b00           // �ж��쳣�ı���
-`define EXC_ADEL            5'h04           // ȡֵ��������쳣�ı���
-`define EXC_ADES            5'h05           // д�����쳣�ı���
-`define EXC_SYS             5'h08           // ϵͳ�����쳣�ı���
-`define EXC_BP              5'h09           // ִ�жϵ�ָ��
-`define EXC_RI              5'h0a           // ����ָ������              
-`define EXC_OV              5'h0c           // ��������쳣�ı���
-`define EXC_NONE            5'h10           // ���쳣
-`define EXC_ERET            5'h11           // ERET�쳣�ı���
-`define EXC_ADDR            32'hbfc00380    // �쳣����������ڵ�ַ
-`define EXC_INT_ADDR        32'hbfc00380    // �ж��쳣����������ڵ�ַ
+`define NOFLUSH             1'b0            
+`define FLUSH               1'b1            
 
-`define NOFLUSH             1'b0            // �������ˮ��
-`define FLUSH               1'b1            // �����쳣�������ˮ��
-/************************�쳣���� end*********************************/
 `define SRAMLIKE_WRITE             	1'b1            
 `define SRAMLIKE_READ              	1'b0
 
